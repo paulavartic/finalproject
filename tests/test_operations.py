dd2eea41296962495a2e70af3ee91ba60eda7205
Paula Vartic <paula.vartic@gmail.com>
2024-05-20 01:24:11 +0200

    test added

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,5 +1,6 @@
-from utils import get_date, hide_acc, hide_card, hide_transaction, organized_operations
+from utils import get_date, hide_acc, hide_card, hide_transaction, pop_up_msg
 import pytest
+import json
 
 def test_get_date():
     assert get_date("2018-07-11T02:26:18.671407") == "11.07.2018"
@@ -20,3 +21,19 @@ def test_hide_card_negative():
 def test_hide_transaction():
     assert hide_transaction("Счет 72731966109147704472") == "Счет **4472"
 
+
+def test_pop_up_msg():
+    assert pop_up_msg({
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }) == "26.08.2019 Перевод организации\nMaestro 1596 83** **** 5199 -> Счет **9589\n31957.58 руб."
